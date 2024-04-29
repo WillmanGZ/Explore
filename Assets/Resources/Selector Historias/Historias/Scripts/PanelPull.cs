045ae0956763d52b42465521d8b884609a8b26f2
sramoscor <dsramos@uninorte.edu.co>
2024-04-29 15:21:40 -0500

    Juego BD y TL actualizado
    Falta agregarles sus repectivos estados de victoria y derrota, ademas, falta el de los 3 cerditos y falta su logo

--- Assets/Resources/Selector Historias/Historias/Scripts/PanelPull.cs
+++ Assets/Resources/Selector Historias/Historias/Scripts/PanelPull.cs
@@ -8,11 +8,36 @@ public class PanelPull : MonoBehaviour, IDropHandler
     public GameObject contenedor;
     public void OnDrop(PointerEventData eventData) //Cada vez que caiga un panel en este contenedor, ese panel se haga hijo de ese contenedor
     {
-        if (transform.childCount == 0) // Verifica si el contenedor no tiene hijos
+        // Obtener el objeto arrastrado
+        GameObject itemDragging = DragHandler.itemDragging;
+
+        // Si no hay un objeto arrastrado, salir del método
+        if (!itemDragging)
+            return;
+
+        // Si el contenedor está vacío, asignar el objeto arrastrado como hijo
+        if (transform.childCount == 0)
         {
-            DragHandler.itemDragging.transform.SetParent(transform);
-            DragHandler.itemDragging.transform.position = transform.position;
-            DragHandler.itemDragging.GetComponent<RectTransform>().sizeDelta = GetComponent<RectTransform>().sizeDelta;
+            itemDragging.transform.SetParent(transform);
+            itemDragging.transform.position = transform.position;
+            itemDragging.GetComponent<RectTransform>().sizeDelta = GetComponent<RectTransform>().sizeDelta;
+        }
+        else // Si el contenedor no está vacío, intercambiar los padres de los objetos
+        {
+            // Obtener el primer hijo del contenedor
+            GameObject currentChild = transform.GetChild(0).gameObject;
+
+            // Intercambiar los padres de los objetos arrastrado y el hijo actual del contenedor
+            Transform startParent = DragHandler.startParent;
+            Transform currentChildParent = currentChild.transform.parent;
+
+            itemDragging.transform.SetParent(currentChildParent);
+            itemDragging.transform.position = currentChildParent.position;
+            itemDragging.GetComponent<RectTransform>().sizeDelta = GetComponent<RectTransform>().sizeDelta;
+
+            currentChild.transform.SetParent(startParent);
+            currentChild.transform.position = startParent.position;
+            currentChild.GetComponent<RectTransform>().sizeDelta = startParent.GetComponent<RectTransform>().sizeDelta;
         }      
     }
 }
