c15c60b16992e95096f704d783d2e0665d304574
sramoscor <dsramos@uninorte.edu.co>
2024-04-30 19:03:01 -0500

    .

--- Assets/Resources/Selector Historias/Historias/Scripts/HubManager.cs
+++ Assets/Resources/Selector Historias/Historias/Scripts/HubManager.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HUB : MonoBehaviour
 {
+    public AudioSource musica;
+     public Sprite boton_musica_off;//Sprite del boton musica apagado
+    public Sprite boton_musica_on;//Sprite del boton musica encendio
+    public Image boton_musica;//Selector del objeto boton musica
     void Awake()
     {
     DestroyObjectByName("Musica Menu");
@@ -52,5 +57,27 @@ public class HUB : MonoBehaviour
 
     public void BotonBack(){
         SceneManager.LoadScene(1);
+    }
+
+     private void ActualizarEstadoMusica()
+    {
+        if (musica.mute)
+        {
+            boton_musica.sprite = boton_musica_off;
+        }
+        else
+        {
+            boton_musica.sprite = boton_musica_on;
+        }
+    }
+
+    public void BotonMusica()
+    {
+        if (musica != null)
+        {
+            // Cambiar el estado de mute y actualizar el sprite del botón
+            musica.mute = !musica.mute;
+            ActualizarEstadoMusica();
+        }
     }
 }
