7960d8635f396a352740dbe9a91ea4e4c807a90b
sramoscor <dsramos@uninorte.edu.co>
2024-05-28 23:32:34 -0500

    Inicio terminado (Sin musica) y Creditos (No estetico)

---
+++ Assets/Resources/Creditos/Scripts/CreditosManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditosManager : MonoBehaviour
+{
+    public Renderer fondo; //Selecciona un material 3D para fondo
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Hace que el objeto 3D se mueva 0.02f unidades en el eje X por segundo
+        fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(0.02f,0)* Time.deltaTime;
+    }
+
+    public void botonBack(){
+        SceneManager.LoadScene(0);
+    }
+}
