4d228e98797bb23ad3a9675c72c2e152d8622ffa
sramoscor <dsramos@uninorte.edu.co>
2024-05-28 23:07:01 -0500

    Juego de historias terminado (Sin musica)

--- Assets/Resources/Selector Historias/Historias/Scripts/HubManager.cs
+++ Assets/Resources/Selector Historias/Historias/Scripts/HubManager.cs
@@ -7,23 +7,10 @@ using UnityEngine.UI;
 
 public class HUB : MonoBehaviour
 {
-    public AudioSource musica;
-     public Sprite boton_musica_off;//Sprite del boton musica apagado
-    public Sprite boton_musica_on;//Sprite del boton musica encendio
-    public Image boton_musica;//Selector del objeto boton musica
-    void Awake()
-    {
-    DestroyObjectByName("Musica Menu");
-    }
-    
     // Start is called before the first frame update
     void Start()
     {
-         if (musica != null)
-        {
-            // Establecer el sprite del botón según el estado de mute
-            ActualizarEstadoMusica();
-        } 
+        
     }
 
     // Update is called once per frame
@@ -32,21 +19,7 @@ public class HUB : MonoBehaviour
         
     }
 
-    void DestroyObjectByName(string nombreObjeto)
-    {
-        GameObject objetoBuscado = GameObject.Find(nombreObjeto);
-
-        if (objetoBuscado != null)
-        {
-            // El objeto fue encontrado, destruirlo
-            Destroy(objetoBuscado);
-        }
-        else
-        {
-            // El objeto no fue encontrado
-            Debug.LogWarning("El objeto con nombre '" + nombreObjeto + "' no se encontró en la escena.");
-        }
-    }
+    
     public void TortugaLiebre(){
         SceneManager.LoadScene(3);
     }
@@ -63,25 +36,4 @@ public class HUB : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
-     private void ActualizarEstadoMusica()
-    {
-        if (musica.mute)
-        {
-            boton_musica.sprite = boton_musica_off;
-        }
-        else
-        {
-            boton_musica.sprite = boton_musica_on;
-        }
-    }
-
-    public void BotonMusica()
-    {
-        if (musica != null)
-        {
-            // Cambiar el estado de mute y actualizar el sprite del botón
-            musica.mute = !musica.mute;
-            ActualizarEstadoMusica();
-        }
-    }
 }
