0e914a13a23d462b43f4754368c2ec3ef604dbd2
sramoscor <dsramos@uninorte.edu.co>
2024-05-29 06:39:59 -0500

    Juego de memoria terminado (No Sonido No Instrucciones))

--- Assets/Resources/Juego de Memoria/Scripts/CardController.cs
+++ Assets/Resources/Juego de Memoria/Scripts/CardController.cs
@@ -10,47 +10,37 @@ public class CardController : MonoBehaviour
     public int MaxCardType => prefabs.Count;
     public UnityEvent<CardController> OnClicked;
 
-     
     public int CardType = -1;
     public float Cardsize = 2f;
     private Animator _animator;
 
     private void Awake()
     {
-       _animator = GetComponent<Animator>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Start()
     {
-        // Nota: Usa el reveal para mostrar las cartas unos segundos antes de ocultarlas todas :) 
         if (CardType < 0)
         {
             CardType = UnityEngine.Random.Range(0, prefabs.Count);
         }
         Instantiate(prefabs[CardType], transform.position, Quaternion.identity, transform);
-
     }
 
     private void OnMouseUpAsButton()
     {
-        OnClicked.Invoke(this);
-    }
-
-    public void testAnimation()
-    {
-        IEnumerator AnimationCoroutine()
+        if (!LevelController.IsInputBlocked)
         {
-            reveal();
-            yield return new WaitForSeconds(2);
-            Hide ();
+            OnClicked.Invoke(this);
         }
-        StartCoroutine(AnimationCoroutine());
     }
 
-    public void reveal()
+    public void Reveal()
     {
         _animator.SetBool("revealed", true);
     }
+
     public void Hide()
     {
         _animator.SetBool("revealed", false);
--- Assets/Resources/Juego de Memoria/Scripts/LevelController.cs
+++ Assets/Resources/Juego de Memoria/Scripts/LevelController.cs
@@ -1,4 +1,3 @@
-using Microsoft.Unity.VisualStudio.Editor;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,81 +7,75 @@ using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour
 {
-
     [SerializeField] private CardController _cardPrefab;
+    [SerializeField] private GameObject MensajeCorrecto;
+    [SerializeField] private GameObject MensajeIncorrecto;
+    [SerializeField] private GameObject Desenfoque;
 
-    //-----------------------------------------------------------------------//
-    [Header("levelData")]                                                    //
-                                                                             //
-    [SerializeField] private int _columns = 4;                               //
-    [SerializeField] private int _rows = 4;                                  //
-    [SerializeField] private int _difficulty = 4;                            //
-    [SerializeField] private int _movements = 12;                            //
-    //-----------------------------------------------------------------------//
+    [Header("levelData")]
+    [SerializeField] private int _columns = 4;
+    [SerializeField] private int _rows = 4;
+    [SerializeField] private int _difficulty = 4;
+    [SerializeField] private int _movements = 12;
 
     [Header("UI")]
     [SerializeField] private Text _movtext;
 
-    ///////////////////////////////////////////////////////////////////////////////
-    private CardController _activeCard;
+    static private CardController _firstCard;
+    static private CardController _secondCard;
     private int _movementsUsed = 0;
-    private bool _blockInput=false;
+    private static bool _blockInput = false;
     private List<CardController> _cards = new List<CardController>();
-    ///////////////////////////////////////////////////////////////////////////////
 
-    public void startlevel()
+    public static bool IsInputBlocked => _blockInput;
+
+    public void StartLevel()
     {
-if (_difficulty > _cardPrefab.MaxCardType)
+        if (_difficulty > _cardPrefab.MaxCardType)
         {
-            _difficulty = Math.Min(_difficulty, _cardPrefab.MaxCardType); // regula la dificultad
+            _difficulty = Math.Min(_difficulty, _cardPrefab.MaxCardType);
             Debug.Assert(false);
-
         }
-        Debug.Assert((_rows * _columns)%2 ==0);
+        Debug.Assert((_rows * _columns) % 2 == 0);
 
         _cards.ForEach(c => Destroy(c.gameObject));
         _cards.Clear();
 
         List<int> allTypes = new List<int>();
-        for(int i =0;i<_cardPrefab.MaxCardType;i++ )
+        for (int i = 0; i < _cardPrefab.MaxCardType; i++)
         {
             allTypes.Add(i);
         }
 
-
-        List<int> gameTypes = new List<int> ();
-        for(int i = 0; i<_difficulty;i++)
+        List<int> gameTypes = new List<int>();
+        for (int i = 0; i < _difficulty; i++)
         {
-            int chosenType = allTypes[UnityEngine.Random.Range(0,allTypes.Count)];
+            int chosenType = allTypes[UnityEngine.Random.Range(0, allTypes.Count)];
             allTypes.Remove(chosenType);
             gameTypes.Add(chosenType);
         }
 
         List<int> chosenTypes = new List<int>();
-        for (int i = 0; i < (_rows*_columns)/2 ; i++)
+        for (int i = 0; i < (_rows * _columns) / 2; i++)
         {
             int chosenType = gameTypes[UnityEngine.Random.Range(0, gameTypes.Count)];
             chosenTypes.Add(chosenType);
             chosenTypes.Add(chosenType);
-    
         }
 
         Vector3 offset = new Vector3((_columns - 1) * _cardPrefab.Cardsize, (_rows - 1) * _cardPrefab.Cardsize, 0f) * 0.5f;
 
-
-        for(int y =0;y<_rows;y++)
-        {                   // Ubica todo
-            for(int x =0;x<_columns;x++)
+        for (int y = 0; y < _rows; y++)
+        {
+            for (int x = 0; x < _columns; x++)
             {
                 Vector3 position = new Vector3(x * _cardPrefab.Cardsize, y * _cardPrefab.Cardsize, 0f);
 
-                var card = Instantiate(_cardPrefab, position - offset, 
-                    Quaternion.identity);
+                var card = Instantiate(_cardPrefab, position - offset, Quaternion.identity);
                 card.CardType = chosenTypes[UnityEngine.Random.Range(0, chosenTypes.Count)];
                 chosenTypes.Remove(card.CardType);
-                card.OnClicked.AddListener(OncardClicked);
+                card.OnClicked.AddListener(OnCardClicked);
                 _cards.Add(card);
-
             }
         }
         _blockInput = false;
@@ -90,42 +83,79 @@ if (_difficulty > _cardPrefab.MaxCardType)
         _movtext.text = $"Movimientos disponibles: {_movements}";
     }
 
-    private void OncardClicked(CardController card)
+    private void OnCardClicked(CardController card)
     {
-        //if (_blockInput)              // NO SIRVE, CAMBIA A TRUE SOLO Y NO DEJA JUGAR
-        //{
-        //    print(_blockInput);
-        //    return;
-        //}                             // para evitar q seleccione mas de 2 
-
-
-        //_blockInput = true;   
-        print(_blockInput + "s");
-
-        if (_activeCard == null)
+        if (_blockInput || card == _firstCard || card == _secondCard)
         {
-            StartCoroutine(selectCard(card));
             return;
         }
 
-        _movementsUsed++;
-        _movtext.text = $"Movimientos disponibles: {_movements - _movementsUsed}";
-        //card.testAnimation();
-
-        if (card.CardType == _activeCard.CardType)
+        if (_firstCard == null)
         {
-            StartCoroutine(score(card));
-            return;
+            _firstCard = card;
+            _firstCard.Reveal();
         }
-        StartCoroutine(fail(card));
+        else if (_secondCard == null)
+        {
+            _secondCard = card;
+            _secondCard.Reveal();
+            StartCoroutine(ProcessCardSelection());
+        }
     }
 
-    public IEnumerator selectCard(CardController card)
+    private IEnumerator ProcessCardSelection()
     {
-        _activeCard = card;
-        _activeCard.reveal();
-       yield return new WaitForSeconds(0.5f);
+        _blockInput = true;
+        yield return new WaitForSeconds(1f);
+
+        if (_firstCard.CardType == _secondCard.CardType)
+        {
+            StartCoroutine(Score(_firstCard, _secondCard));
+        }
+        else
+        {
+            StartCoroutine(Fail(_firstCard, _secondCard));
+        }
+        _movementsUsed += 1;
+        _movtext.text = $"Movimientos disponibles: {_movements - _movementsUsed}";
+    }
+
+    private IEnumerator Score(CardController firstCard, CardController secondCard)
+    {
+        yield return new WaitForSeconds(1f);
+        _cards.Remove(firstCard);
+        Destroy(firstCard.gameObject);
+        _cards.Remove(secondCard);
+        Destroy(secondCard.gameObject);
+
+        _firstCard = null;
+        _secondCard = null;
         _blockInput = false;
+
+        if (_cards.Count < 1)
+        {
+            Win();
+        }
+        else if (_movementsUsed >= _movements)
+        {
+            Lose();
+        }
+    }
+
+    private IEnumerator Fail(CardController firstCard, CardController secondCard)
+    {
+        yield return new WaitForSeconds(1f);
+        firstCard.Hide();
+        secondCard.Hide();
+
+        _firstCard = null;
+        _secondCard = null;
+        _blockInput = false;
+
+        if (_movementsUsed >= _movements)
+        {
+            Lose();
+        }
     }
 
     void Start()
@@ -134,121 +164,19 @@ if (_difficulty > _cardPrefab.MaxCardType)
         _columns = PlayerPrefs.GetInt("_columns");
         _movements = PlayerPrefs.GetInt("_movements");
         _difficulty = PlayerPrefs.GetInt("_difficulty");
-        startlevel();
-
+        StartLevel();
     }
 
-    void Update()
+    private void Win()
     {
-        
-    }
-    
-    /*private IEnumerator score(CardController card)
-    {
-    card.reveal();
-        yield return new WaitForSeconds(0.5f);
-        _cards.Remove(_activeCard);
-        _cards.Remove(card);
-        Destroy(card.gameObject);
-        Destroy(_activeCard.gameObject);
-        _activeCard=null;
-        _blockInput = false;
-        if(_cards.Count <1)
-        {
-            win();
-            yield break;
-        }
-        if (_movementsUsed >= _movements)
-        {
-            lose();
-            yield break;
-        }
-        _blockInput = false;
-    } */
-
-    private IEnumerator score(CardController card)
-{
-    card.reveal();
-    yield return new WaitForSeconds(0.5f);
-
-    if (_activeCard != null) 
-    {
-        _cards.Remove(_activeCard);
-        Destroy(_activeCard.gameObject);
-    }
-    
-    if (card != null)
-    {
-        _cards.Remove(card);
-        Destroy(card.gameObject);
+        Desenfoque.SetActive(true);
+        MensajeCorrecto.SetActive(true);
     }
 
-    _activeCard = null;
-    _blockInput = false;
-
-    if (_cards.Count < 1)
+    private void Lose()
     {
-        win();
-        yield break;
-    }
-    if (_movementsUsed >= _movements)
-    {
-        lose();
-        yield break;
-    }
-    _blockInput = false;
-}
-
-
-    /* Antigua funcion fail, fue cambiada por una donde se compruebe si la carta es null, pero se deja en caso de fallos
-    private IEnumerator fail(CardController card)
-    {
-        card.reveal();
-        yield return new WaitForSeconds(1f);
-        _activeCard.Hide();
-        card.Hide();
-        _activeCard = null;
-        yield return new WaitForSeconds(0.5f);
-        _blockInput = false;
-        if (_movementsUsed >= _movements)
-        {
-            lose();
-            yield break;
-        }
-
-
-    }
-    */
-
-    private IEnumerator fail(CardController card)
-{
-    card.reveal();
-    yield return new WaitForSeconds(1f);
-    if (_activeCard != null) // Verifica si _activeCard no es null
-    {
-        _activeCard.Hide();
-    }
-    card.Hide();
-    _activeCard = null;
-    yield return new WaitForSeconds(0.5f);
-    _blockInput = false;
-    if (_movementsUsed >= _movements)
-    {
-        lose();
-        yield break;
-    }
-}
-
-    private void win()
-    {
-        Debug.Log("ganaste");   // Mostrar un panel q diga q ganaste
-    }
-
-    private void lose()
-    {
-        Debug.Log("perdiste");
-        //_gameOverButton.SetActive(true);
-
+        Desenfoque.SetActive(true);
+        MensajeIncorrecto.SetActive(true);
     }
 
     public void Back()
@@ -257,7 +185,10 @@ if (_difficulty > _cardPrefab.MaxCardType)
     }
 }
 
-// pendientes:
-// 1) Arregla lo del booleano
-// 2) mensaje de victoria y puntos 
-// 3) UI
+
+
+
+
+
+
+
